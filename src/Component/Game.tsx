0e899c36bc3678a4572931fc437ac8f08f3995b1
petrescu <79636369+petrescu14@users.noreply.github.com>
2022-05-31 01:24:30 +0900

    履歴改善

--- src/Component/Game.tsx
+++ src/Component/Game.tsx
@@ -9,29 +9,42 @@ const Game = () => {
   const [stepNumber, setStepNumber] = useState(0)
   const winner = calculateWinner(squares)
   const status: string = winner ? "Winner" + winner : "Next player: " + (xIsNext ? "X" : "O");
+  const [handHistory, setHandHistory] = useState([{ hands: Array(2).fill(0) }])
 
-
+  const highlightHistory = (i: number) => {
+    const num = handHistory.findIndex((handHistory, index) => (handHistory.hands === [Math.floor(i / 3), i % 3]))
+    return num
+  }
 
   const handleClick = (i: number) => {
     if (calculateWinner(squares) || squares[i]) return
-
-    const new_history = history
     const new_squares: string[] = squares.map((value, index) => (
       index === i ? (xIsNext ? "X" : "O") : value
     ))
     setSquares(new_squares)
     setXIsNext(!xIsNext)
-    setHistory([...new_history, { squares: new_squares }])
+    setHistory([...history, { squares: new_squares }])
     setStepNumber(stepNumber + 1)
+    setHandHistory([...handHistory, { hands: [Math.floor(i / 3) + 1, i % 3 + 1] }])
   }
 
   const jumpTo = (step: number) => {
     const new_history = history.filter((history, index) => (index <= step))
     const new_squares = step ? new_history[step].squares : Array(9).fill("")
+    const new_handHistory = handHistory.filter((handHistory, index) => (index <= step))
     setSquares(new_squares)
     setHistory(new_history)
     setStepNumber(step)
     setXIsNext(step % 2 === 0)
+    setHandHistory(new_handHistory)
+  }
+
+  const handDesc = (step: number) => {
+    if (step !== 0) {
+      return (
+        "(" + handHistory[step].hands[0] + "," + handHistory[step].hands[1] + ") sets by Player " + (step % 2 ? "X" : "O")
+      )
+    }
   }
 
   const moves = history.map((step, move) => {
@@ -43,6 +56,7 @@ const Game = () => {
         <button
           onClick={() => jumpTo(move)}
         >{desc}</button>
+        {handDesc(move)}
       </li>
     )
   })
@@ -52,7 +66,10 @@ const Game = () => {
       <div className="game-board">
         <Board
           squares={squares}
-          onClick={(i: number) => handleClick(i)}
+          onClick={(i: number) => {
+            handleClick(i)
+            highlightHistory(i)
+          }}
         />
       </div>
       <div className="game-info">
